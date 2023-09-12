34d307ab64785470af44a6e8c2a9fb9907610dea
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-09-12 23:34:27 +0600

    DB connection created. Model Product created

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='catalog/images', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey
+    price = models.DecimalField(verbose_name='Цена')
+    created_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    modified_date = models.DateField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.name}/{self.category} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name',)
