8bd886be4a174b0d2fd4949cbb50dbf36a9105ee
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-09-13 00:14:37 +0600

    Model Category created

--- catalog/models.py
+++ catalog/models.py
@@ -6,9 +6,9 @@ NULLABLE = {'blank': True, 'null': True}
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='catalog/images', verbose_name='Изображение', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey
-    price = models.DecimalField(verbose_name='Цена')
+    price = models.PositiveIntegerField(verbose_name='Цена')
     created_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     modified_date = models.DateField(auto_now=True, verbose_name='Дата изменения')
 
@@ -18,4 +18,17 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+        ordering = ('name',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
         ordering = ('name',)
--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in product ion secret!
 SECRET_KEY = 'django-insecure-3bg(#$&whn+($^2og(zxl-+4ki952p4f(ywfz4f9**k4hx!g8n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
