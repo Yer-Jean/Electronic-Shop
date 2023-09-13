42ecbcfe6f9437c11dadc84e0cced294a8f357eb
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-09-13 22:45:06 +0600

    Models extended. Admin classes added

--- catalog/models.py
+++ catalog/models.py
@@ -1,13 +1,28 @@
+from datetime import date
+
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание')
+    # created_at = models.DateField(default=date.today, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category = models.ForeignKey
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', default=0)
     price = models.PositiveIntegerField(verbose_name='Цена')
     created_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     modified_date = models.DateField(auto_now=True, verbose_name='Дата изменения')
@@ -18,17 +33,4 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
-        ordering = ('name',)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-
-    def __str__(self):
-        return f'{self.name}'
-
-    class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
         ordering = ('name',)
