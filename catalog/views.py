8e38932a4bb5e5f978b36c07fa81bd70945786d7
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-09-16 18:53:59 +0600

    New templates applied

--- catalog/views.py
+++ catalog/views.py
@@ -4,14 +4,24 @@ from utils.json_saver import write_to_json_file
 
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        # 'object_list': Category.objects.all(),  # вывод всех объектов
+        'title': 'Easy shopping with Dream',
+        'sub_title': 'Explore our gadgets catalog'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
+    context = {
+        # 'object_list': Category.objects.all(),  # вывод всех объектов
+        'title': 'Our Contacts',
+        'sub_title': ''
+    }
     data = []
     if request.method == 'POST':
         data.append({'name': request.POST.get('name'),
                      'email': request.POST.get('email'),
                      'message': request.POST.get('message')})
         write_to_json_file('messages.json', data)
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
