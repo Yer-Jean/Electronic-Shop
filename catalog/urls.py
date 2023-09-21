bb551ff4e7fe966496ec8950a79d1221ae469bd9
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-09-21 19:43:21 +0600

    FBV -> CBV refactoring. Category list added

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from . import views
 from .apps import CatalogConfig
+from .views import CategoryListView, ProductListView, ProductDetailView, IndexView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:pk>/product/', views.product, name='product'),
+    path('', IndexView.as_view(), name='index'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('contacts/', views.contacts),
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('products/<int:pk>/', ProductListView.as_view(), name='category'),
 ]
