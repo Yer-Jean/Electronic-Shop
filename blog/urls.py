a7e8505f9b5e0b52e3f3569fe7aa6d9cbf695a62
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-09-22 17:30:14 +0600

    Article Add and Delete views and forms created

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 from .apps import BlogConfig
-from .views import ArticleListView
-
+from .views import ArticleListView, ArticleCreateView, ArticleDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('articles/', ArticleListView.as_view(), name='articles')
+    path('articles/', ArticleListView.as_view(), name='articles'),
+    path('article/create/', ArticleCreateView.as_view(), name='article_create'),
+    path('article/delete/<int:pk>', ArticleDeleteView.as_view(), name='article_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Article
 
@@ -11,6 +13,7 @@ class ArticleListView(ListView):
         'sub_title': ''
     }
 
+
 class ArticleDetailView(DetailView):
     model = Article
 
@@ -19,3 +22,27 @@ class ArticleDetailView(DetailView):
         self.object.views_count += 1
         self.object.save()
         return self.object
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ('title', 'content', 'image',)
+    success_url = reverse_lazy('blog:articles')
+    extra_context = {
+        'title': 'Dream shop - Blog',
+        'sub_title': 'Add article'
+    }
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_article = form.save()
+            # slugify импортируем из pytils - поддержка русского языка - нужно установить
+            new_article.slug = slugify(new_article.title)
+            new_article.save()
+
+        return super().form_valid(form)
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = reverse_lazy('blog:articles')
