230f0a2773bc638d724792b52a53953e7b13c61e
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-09-23 01:00:53 +0600

    Small bug fixed with Contacts url

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,7 +9,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
-    path('contacts/', views.contacts),
+    path('contacts/', views.contacts, name='contacts'),
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('products/<int:pk>/', ProductListView.as_view(), name='category'),
 ]
