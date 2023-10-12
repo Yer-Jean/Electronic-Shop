03cb4686b4de53478353a82ac62c2291be84eaf4
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-10-12 14:20:52 +0600

    Working with Versions with Generics realized

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,26 +1,52 @@
 from django import forms
+from django.shortcuts import render
 
-from catalog.models import Product
+from catalog.models import Product, Version
 from config.settings import WORDS_BLACK_LIST
 from utils.utils import text_to_set_without_punctuation
 
 
-class ProductForm(forms.ModelForm):
-
-    class Meta:
-        model = Product
-        fields = '__all__'
+class StyleFormMixin:
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for filed_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+        for field_name, field in self.fields.items():
+            if field_name != 'is_active':
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
 
     def clean(self):
         cleaned_data = super().clean()
         for field in ['name', 'description']:
             cleaned_field_data = cleaned_data.get(field)
-            common_words: set = text_to_set_without_punctuation(cleaned_field_data) & WORDS_BLACK_LIST
+            text = text_to_set_without_punctuation(cleaned_field_data)
+            if text:
+                common_words: set = text & WORDS_BLACK_LIST
+            else:
+                break
             if len(common_words) > 0:
                 raise forms.ValidationError(f'Incorrect words used: {", ".join(common_words)}')
         return cleaned_data
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
+
+    def clean_is_active(self):
+        is_active = self.cleaned_data['is_active']
+        product = self.cleaned_data['product']
+        # product = self.instance.product  # Получаем связанный продукт
+        if is_active:
+            # Проверяем, есть ли другие активные версии этого продукта
+            active_versions_count = Version.objects.filter(product=product, is_active=True).exclude(
+                id=self.instance.id).count()
+            if active_versions_count > 0:
+                raise forms.ValidationError('Уже есть активная версия этого продукта.')
+        return is_active
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,9 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
-from catalog.forms import ProductForm
+from catalog.forms import ProductForm, VersionForm
 from catalog.models import Product, Category, Version
 from utils.json_saver import write_to_json_file
 
@@ -86,16 +87,75 @@ class ProductUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
 
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+        context_data['formset'] = formset
+
         product_item = Product.objects.get(pk=self.kwargs.get('pk'))
         context_data['product_pk'] = product_item.pk
         context_data['title'] = f'Edit - {product_item.name}'
 
         return context_data
 
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
+
 
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:index')
+
+
+class VersionListView(ListView):
+    model = Version
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(product_id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        product_item = Product.objects.get(pk=self.kwargs.get('pk'))
+        context_data['product_pk'] = product_item.pk
+        context_data['title'] = f'{product_item.name}'
+        context_data['sub_title'] = 'Version history'
+
+        return context_data
+
+
+class VersionUpdateView(UpdateView):
+    model = Version
+    form_class = VersionForm
+
+    def get_success_url(self):
+        return reverse('catalog:version_list', args=[self.object.product_id])
+
+
+class VersionDeleteView(DeleteView):
+    model = Version
+
+    def get_success_url(self):
+        return reverse('catalog:version_list', args=[self.object.product_id])
+
+
+class VersionCreateView(CreateView):
+    model = Version
+    form_class = VersionForm
+
+    def get_success_url(self):
+        return reverse('catalog:version_list', args=[self.object.product_id])
 
 
 def contacts(request):
